62180f9a0f2d0dbc0c9c88ec6a0a95818dfe5f99
patsyotta <patsyotta@gmail.com>
2026-09-04 19:31:28 -0700

    Actualizar Patsy Finance V11.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='patsy-finance-v11-1';
+const CACHE='patsy-finance-v11-6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{ if(e.request.method==='GET') e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,y)); return x;}).catch(()=>caches.match('./index.html')))); });
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}).catch(()=>caches.match('./index.html'))));});
